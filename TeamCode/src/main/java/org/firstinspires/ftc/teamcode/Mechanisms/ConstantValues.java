cba7361889ab0011780ce2fdb8b9ee6d3de2f96c
Ranveer1010 <ranveer.nagar2011@gmail.com>
2025-11-28 17:13:21 -0800

    Added mec drivebase

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms/ConstantValues.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms/ConstantValues.java
@@ -4,5 +4,7 @@ public class ConstantValues {
     public static final double intakeMax = 1.0;
     public static final double intakeStop = 0.0;
     public static final double intakeReverse = -1.0;
+    public static final double driveMaxPower = 1.0;
+    public static double driveMaxSpeed = 1.0;
 
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOp.java
@@ -1,4 +1,47 @@
 package org.firstinspires.ftc.teamcode.OpModes;
 
-public class TeleOp {
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.Mechanisms.ConstantValues;
+import org.firstinspires.ftc.teamcode.Mechanisms.IntakeConfig;
+import org.firstinspires.ftc.teamcode.Mechanisms.MecanumDrivebase;
+
+public class TeleOp extends LinearOpMode {
+    MecanumDrivebase drive = new MecanumDrivebase();
+    double forward, strafe, rotate;
+    IntakeConfig intake = new IntakeConfig();
+    public void setOperator(){
+        if(gamepad2.left_trigger > 0.7){
+            intake.intakeMax();
+        }else if(gamepad2.dpad_up){
+            intake.reverseIntake();
+        }else{
+            intake.intakeStop();
+        }
+    }
+    public void setDriver(){
+        forward = -gamepad1.left_stick_y;
+        strafe = gamepad1.left_stick_x;
+        rotate = gamepad1.right_stick_x;
+        drive.driveFieldRelative(forward, strafe, rotate);
+
+        if(gamepad1.right_trigger >= 0.8) {
+            ConstantValues.driveMaxSpeed = 0.3;
+        } else {
+            ConstantValues.driveMaxSpeed = 1.0;
+        }
+    }
+
+    @Override
+    public void runOpMode() throws InterruptedException{
+        telemetry.addLine("Initialized");
+        intake.init(hardwareMap);
+        waitForStart();
+        while(opModeIsActive() && !isStopRequested()){
+            telemetry.addLine("OpMode is active");
+            setOperator();
+            setDriver();
+        }
+    }
+
 }
